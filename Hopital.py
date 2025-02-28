7c54d902f2d82332ef38350f4f600f3739872162
Maxence <2483616@cegepoutaouais.qc.ca>
2025-02-28 10:37:31 -0500

    changement

--- Hopital.py
+++ Hopital.py
@@ -5,10 +5,11 @@ class Hopital:
     """
     classes Hopital
     """
-    def __init__(self, nom : str, adresse : str, type_soins_offert : list):
+    def __init__(self, nom : str, adresse : str, patient : list[Patient], type_soins_offert : list):
         self.nom = nom
         self.adresse = adresse
-        self.type_soins_offert = type_soins_offert
+        self.patient = patient
+        self.type = type_soins_offert
 
     @property
     def nom(self):
@@ -27,19 +28,32 @@ class Hopital:
         self._adresse = adresse
 
     @property
+    def patient(self):
+        return self._patient
+
+    @patient.setter
+    def patient(self, patient):
+        self._patient = patient
+
+    @property
     def type_soins_offert(self):
         return self._type_soins_offert
 
     @type_soins_offert.setter
-    def type_soins_offert(self, type_soins_offer):
-        self._type_soins_offert = type_soins_offer
+    def type(self, type_soins_offer):
+        if type_soins_offer in ["urgent", "moins urgent", "normal", "enfant"]:
+            self._type_soins_offert = type_soins_offer
 
 
 def verifier_besoins(self, patient: Patient):
     """
-    Vérifier  les besoins du patient en verifiant la maladie est ressortant le soin necessaire.
+    Vérifier  les besoins du patient et l'envoyer à l'hopital approprié.
     :param self: Hospital
     :param patient: Patient donner
     :return: soins necessaire
     """
     maladie_p : list = patient.maladies
+    age_p : int = patient.age
+
+
+
